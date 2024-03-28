befea0f8ab62197b1882c77d1403031c469b96dc
Romain Bernier <romain.bernier@live.fr>
2024-03-29 00:48:10 +0100

    added more css, changed post controller to create new post by scrapping

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,47 @@
 require "open-uri"
 require "nokogiri"
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
   def index
     @posts = Post.all
+    @post = Post.new
   end
 
   def show
-    @post = Post.find(params[:id])
     @comments = @post.comments
     @user = current_user
 
     html_file = URI.open(@post.url).read
     html_doc = Nokogiri::HTML.parse(html_file)
     @bigger_photo = html_doc.at_css(".post-header__image-container .image__img").attribute("src").value
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.url.include?("www.bbcgoodfood.com/recipes/")
+      html_file = URI.open(@post.url).read
+      html_doc = Nokogiri::HTML.parse(html_file)
+      @post.title = html_doc.at_css(".post-header__title .heading-1").text
+      @post.content = html_doc.at_css(".editor-content").text
+      @post.photo = html_doc.at_css(".post-header__image-container .image__img").attribute("src").value
+      @post.user = current_user
+      if @post.save
+        redirect_to @post, notice: "Recipe article referenced successfully!"
+      else
+        redirect_to @posts, notice: "Invalid recipe article url!"
+      end
+    else
+      redirect_to posts_path, notice: "Invalid recipe article url!"
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :url)
   end
 end
