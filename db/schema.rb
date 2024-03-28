4ff81f52286074e7bec6e2228c4459e9f8cbe8f8
Romain Bernier <romain.bernier@live.fr>
2024-03-28 22:36:25 +0100

    added photo migration, first scrapping, changed controller to scrap bigger image, various scss

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_28_140538) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_28_193727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_28_140538) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -30,11 +30,13 @@ html_doc.search(".layout-md-rail__primary .card").each do |element|
   title = element.at_css(".card__content a").text.strip
   recipe_url = "https://www.bbcgoodfood.com"+element.at_css(".card__content a").attribute("href").value
   content = element.at_css(".card__content p").inner_html
+  photo_url = element.at_css(".image__img").attribute("src").value
   Post.create!(
     user: admin,
     title: title,
     url: recipe_url,
-    content: content
+    content: content,
+    photo: photo_url
   )
 end
 
